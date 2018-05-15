5ad8c78547abed90ce4617142940245aea251e75
Tom <laurence@growcyber.com>
2018-05-15 19:11:15 +0100

    Updated .gitignore to ignore .db, and removed line count

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -5,24 +5,20 @@ import sys
 from datetime import datetime as dt
 
 #Dumps the output from the GPS sensor into a csv file
-def gps_capture(x=10):
-	'''Takes 1 argument - number of GPS fixes to quit after - defaults 10.
-	Dumps the output from the GPS sensor into a csv file.
+def gps_capture():
+	'''Dumps the output from the GPS sensor into a csv file.
 	Restarts the GPS, and saves the event log csv file one line at a 
 	time so that it still works if the power is cut.'''
 	
 	os.system("sudo killall gpsd")
 	os.system("sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock")
 		
-	hdr = ['Time', 'Lat', 'Lon', 'Alt', 'Speed', 'Heading', 'Journey_ID']
 	j_id = str(dt.now())
 	
 	session = gps.gps("localhost", "2947")
 	session.stream (gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-	row_count = 0
-
-	while row_count < x:
+	while True:
 		try:
 			report = session.next()
 			print report
@@ -44,11 +40,7 @@ def gps_capture(x=10):
 						csv_w = csv.writer(f)
 						csv_w.writerow(row)
 					
-					row_count += 1
-	
 		except KeyboardInterrupt:
 			quit()
 			
 gps_capture()
-	
-	
