632781916f3c18bbe58d6bcb7ac0015d7920b7a4
Tom <tomjenkins66@gmail.com>
2018-05-17 20:12:26 +0100

    Updated get_sensor_data.py without unnecessary imports

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -1,28 +1,20 @@
 import gps
 import csv
-import os
-import sys
 from datetime import datetime as dt
 
 #Dumps the output from the GPS sensor into a csv file
 def gps_capture():
 	'''Dumps the output from the GPS sensor into a csv file.
-	Restarts the GPS, and saves the event log csv file one line at a 
+	Saves the event log csv file one line at a 
 	time so that it still works if the power is cut.'''
 	
-	os.system("sudo killall gpsd")
-	os.system("sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock")
-		
 	j_id = str(dt.now())
-	
 	session = gps.gps("localhost", "2947")
 	session.stream (gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
 	while True:
 		try:
 			report = session.next()
-			print report
-				
 			if report['class'] == 'TPV':
 				if hasattr(report, 'time'):
 					print 'Lat:', report.lat
@@ -39,8 +31,6 @@ def gps_capture():
 					with open('Session.csv', 'a+') as f:
 						csv_w = csv.writer(f)
 						csv_w.writerow(row)
-					
 		except KeyboardInterrupt:
 			quit()
-			
 gps_capture()
